a84f3d1b80539439afce704d395f0155461382da
github username <purnima.nyc@gmail.com>
2019-02-13 16:30:04 -0500

    add header text

--- react-client/src/Components/Search.js
+++ react-client/src/Components/Search.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-
+  render () {
+    return (
+      <div>
+        <h1 className="welcome">Welcome to the Books Search App!</h1>
+      </div>
+    )
+  }
 }
 
 export default Search;
