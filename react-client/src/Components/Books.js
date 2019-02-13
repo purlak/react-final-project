37cc972b75d5b74d288e412ee2563ff195dcb221
github username <purnima.nyc@gmail.com>
2019-02-13 18:32:27 -0500

    add check for props

--- react-client/src/Components/Books.js
+++ react-client/src/Components/Books.js
@@ -3,11 +3,19 @@ import React, { Component } from 'react';
 class Books extends Component {
 
   render () {
-    return (
-      <div>
-      </div>
-    )
+    if (typeof(this.props.books) === 'undefined'){
+      return (<h3>No books found. Try again.</h3>)
+    }
+    else if (typeof(this.props.books) !== 'undefined' || this.props.books !== null){
+      return (
+        <div>
+          {this.props.books.map(book =>
+            <div className="displayBook">
+              <Display book= {book}/>
+            </div>
+          )}
+          </div>
+      )}
   }
 }
-
 export default Books;
