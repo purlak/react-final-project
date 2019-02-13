b4865f91f830950042c9c16b1b7902c4b26a7009
github username <purnima.nyc@gmail.com>
2019-02-13 16:35:21 -0500

    add event handler

--- react-client/src/Components/Search.js
+++ react-client/src/Components/Search.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+
+  handleSearchInput = () => {
+
+  }
+  
   render () {
     return (
       <div>
         <input
           type="text"
-          onChange=""
+          onChange={this.handleSearchInput}
           onKeyPress=""
           value=""
           placeholder=""
