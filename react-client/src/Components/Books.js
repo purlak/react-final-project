a3ba2a42909e0abf62b41f53665b41efb18f0d19
github username <purnima.nyc@gmail.com>
2019-02-13 21:02:49 -0500

    refactor component

--- react-client/src/Components/Books.js
+++ react-client/src/Components/Books.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-// import Display from './Display.js';
-import '../css/DisplayBooks.css';
+import Display from './Display.js';
 import No_image_available from '../images/No_image_available.png';
 
 class Books extends Component {
@@ -12,30 +11,8 @@ class Books extends Component {
     else if (typeof(this.props.books) !== 'undefined' || this.props.books !== null){
       return (
         <div>
-          {this.props.books.map(book =>
-            <div className="displayBook">
-              {book.volumeInfo.imageLinks? (
-                <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
-              ) : (
-                <img src={`${No_image_available}`} className="imageSize"/>
-              )}
-
-              {book.volumeInfo.title? (
-                  <h3 className="displayInfo">{book.volumeInfo.title}</h3>
-              ) : (
-                  <h3 className="displayInfo">Title: Info N/A</h3>
-              )}
-
-              {book.volumeInfo.authors? (
-                  <p className="displayInfo">Author: {book.volumeInfo.authors[0]}</p>
-              ) : (
-                  <p>Author: N/A</p>
-              )}
-
-              <button>Add to MyList</button>
-            </div>
-          )}
-          </div>
+          {this.props.books.map(book => <Display book={book} />)}
+        </div>
       )}
   }
 }
