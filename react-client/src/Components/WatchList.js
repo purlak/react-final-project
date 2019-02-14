6eaf774a820080647259601f6026c710e63c12f7
github username <purnima.nyc@gmail.com>
2019-02-14 00:35:53 -0500

    build watchlist component to display props

--- react-client/src/Components/WatchList.js
+++ react-client/src/Components/WatchList.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// const watchList = [
+//   {title: ""}
+// ]
 
 class WatchList extends Component {
   render () {
-    
+    const { watchList } = this.props
+    return (
+      <div>
+        {watchList.map(book)}
+      </div>
+    )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    watchList: state.watchList
+  }
+}
+export default connect(mapStateToProps)WatchList;
