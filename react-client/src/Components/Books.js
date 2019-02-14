be2f5ba7f2f34fff4dafc39799f0ac15299fbdb1
github username <purnima.nyc@gmail.com>
2019-02-13 20:18:59 -0500

    update to check data + add button

--- react-client/src/Components/Books.js
+++ react-client/src/Components/Books.js
@@ -19,6 +19,20 @@ class Books extends Component {
                 <img src={`${No_image_available}`} className="imageSize"/>
               )}
 
+              {book.volumeInfo.title? (
+                  <h3 className="displayInfo">{book.volumeInfo.title}</h3>
+                ) : (
+                  <h3 className="displayInfo">Title: Info N/A</h3>
+                )}
+
+                {book.volumeInfo.authors? (
+                  <p className="displayInfo">Author: {book.volumeInfo.authors[0]}</p>
+                ) : (
+                  <p>Author: N/A</p>
+                )}
+
+                <button>Add to MyList</button>
+
             </div>
           )}
           </div>
