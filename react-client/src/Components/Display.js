4573b1a79f2df8083bb5cdf727f86de1fd31ed04
github username <purnima.nyc@gmail.com>
2019-02-13 19:17:37 -0500

    assign props to a variable

--- react-client/src/Components/Display.js
+++ react-client/src/Components/Display.js
@@ -4,8 +4,11 @@ import React, { Component } from 'react';
 class Display extends Component {
 
   render () {
+    const { volumeInfo } = this.props.book
     return (
-      
+
     )
   }
 }
+
+export default Display;
