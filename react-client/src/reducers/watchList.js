68fc930c94c8f650b9fbf68384d3d55f7c8576d5
github username <purnima.nyc@gmail.com>
2019-02-14 12:41:24 -0500

    no change

--- react-client/src/reducers/watchList.js
+++ react-client/src/reducers/watchList.js
@@ -1,7 +1,7 @@
 export default (state=[], action) => {
   switch(action.type) {
     case 'FETCH_WATCHLIST_SUCCESS':
-      return action.payload
+      return action.payload;
     default:
       return state;
   }
