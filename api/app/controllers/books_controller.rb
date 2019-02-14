e13072783bef16735d0297781eee5b59b8ecd40a
github username <purnima.nyc@gmail.com>
2019-02-13 20:54:04 -0500

    setup rails api + create action in the controller

---
+++ api/app/controllers/books_controller.rb
@@ -0,0 +1,14 @@
+class BooksController < ApplicationController
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render json: book
+    end 
+  end
+
+  private
+  def book_params
+    params.require[:book].permit(:title, :author, :publisher, :imageLinks)
+  end
+end
