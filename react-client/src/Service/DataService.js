6704b6ad07972000565887039d60760fa3229679
github username <purnima.nyc@gmail.com>
2019-02-13 19:43:34 -0500

    remove whitespace

--- react-client/src/Service/DataService.js
+++ react-client/src/Service/DataService.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import '../css/DisplayBooks.css';
 
-
 const DataService = {
   searchBook (data, key) {
     switch (key)  {
-
-    case ('title'):
-      return (
-        <div> <h2 className="displayInfo">Title: {data}</h2> </div>
-      )
-    case ('authors'):
-      return (
-        <div> <h2 className="displayInfo">Author: {data[0]}</h2> </div>
-      )
-    case ('imageLinks'):
-      return (
-        <div> <img src={`${data.smallThumbnail}`} className="imageSize"/> </div>
-      )
-    default:
-      return;
+      case ('title'):
+        return (
+          <div> <h2 className="displayInfo">Title: {data}</h2> </div>
+        )
+      case ('authors'):
+        return (
+          <div> <h2 className="displayInfo">Author: {data[0]}</h2> </div>
+        )
+      case ('imageLinks'):
+        return (
+          <div> <img src={`${data.smallThumbnail}`} className="imageSize"/> </div>
+        )
+      default:
+        return;
     }
   }
-
-
 }
 
 export default DataService;
