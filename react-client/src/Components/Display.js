86bab45a4fbaf04af3c12d22a5771f582ebc0b2e
github username <purnima.nyc@gmail.com>
2019-02-13 19:24:41 -0500

    import data service component

--- react-client/src/Components/Display.js
+++ react-client/src/Components/Display.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-
+import DataService from '../Service/DataService.js';
 
 class Display extends Component {
 
   getOrDefault = (key, volumeInfo) => {
     if (volumeInfo[key]) {
-      // return DataService.searchBook(volumeInfo[key], key)
+      return DataService.searchBook(volumeInfo[key], key)
     }
     return (
-      // <h2 className="displayInfo">{ key.charAt(0).toUpperCase() + key.slice(1)}: N/A</h2>
+      <h2 className="displayInfo">{ key.charAt(0).toUpperCase() + key.slice(1)}: N/A</h2>
     )
   }
 
