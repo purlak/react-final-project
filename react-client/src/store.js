42dfd0314340b60258a0a0aa1ac06c8b0608d983
github username <purnima.nyc@gmail.com>
2019-02-14 00:51:30 -0500

    add redux thunk

--- react-client/src/store.js
+++ react-client/src/store.js
@@ -1,8 +1,9 @@
-import {  createStore, combineReducers  } from 'redux';
+import {  createStore, combineReducers, applyMiddleware  } from 'redux';
+import thunk from 'redux-thunk'
 import watchList from './reducers/watchList.js'
 
 const rootReducer = combineReducers({
   watchList: watchList
 })
 
-export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
