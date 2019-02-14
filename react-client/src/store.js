4c6e510b97bf7741d09490995e1da8fe8835ca67
github username <purnima.nyc@gmail.com>
2019-02-14 00:45:32 -0500

    import reducer and setup export

--- react-client/src/store.js
+++ react-client/src/store.js
@@ -1,5 +1,8 @@
 import {  createStore, combineReducers  } from 'redux';
+import watchList from './reducers/watchList.js'
 
 const rootReducer = combineReducers({
   watchList: watchList
 })
+
+export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
