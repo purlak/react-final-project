998b1432710d14b426b4ee3f1baf915d94aa06f3
github username <purnima.nyc@gmail.com>
2019-02-14 13:38:14 -0500

    update switch statement for new action

--- react-client/src/reducers/watchList.js
+++ react-client/src/reducers/watchList.js
@@ -2,6 +2,8 @@ export default (state=[], action) => {
   switch(action.type) {
     case 'FETCH_WATCHLIST_SUCCESS':
       return action.payload;
+    case 'ADD_BOOK':
+      return state.concat(action.payload)
     default:
       return state;
   }
