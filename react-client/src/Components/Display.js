541105dc4bb13275e737605f27ead4f57ce6e10f
github username <purnima.nyc@gmail.com>
2019-02-14 12:08:38 -0500

    clean up code + remove whitespace

--- react-client/src/Components/Display.js
+++ react-client/src/Components/Display.js
@@ -4,12 +4,10 @@ import No_image_available from '../images/No_image_available.png';
 
 class Display extends Component {
   addBook = () => {
-    // console.log(this.props.book)
     const {title, publisher, imageLinks} = this.props.book.volumeInfo
     const bookObj = {
       title, publisher, imageLinks
     }
-    console.log(bookObj)
     fetch (`http://localhost:3001/books`, {
       method: 'POST',
       body: JSON.stringify({book: bookObj}),
@@ -27,13 +25,11 @@ class Display extends Component {
           ) : (
             <img src={`${No_image_available}`} className="imageSize"/>
           )}
-
           {book.volumeInfo.title? (
               <h3 className="displayInfo">{book.volumeInfo.title}</h3>
           ) : (
               <h3 className="displayInfo">Title: Info N/A</h3>
           )}
-
           {book.volumeInfo.authors? (
               <p className="displayInfo">Author: {book.volumeInfo.authors[0]}</p>
           ) : (
