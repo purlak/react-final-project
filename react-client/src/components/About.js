bde33571afdfde3422ec2abaf16f1a46d85fbdda
github username <purnima.nyc@gmail.com>
2019-02-23 10:43:05 -0500

    add new component

--- react-client/src/components/About.js
+++ react-client/src/components/About.js
@@ -1,1 +1,12 @@
 import React, {Component} from 'react';
+
+class About extends Component {
+  render () {
+
+    return (
+      <h1 className="WatchList">Hello</h1>
+    )
+  }
+}
+
+export default About;
