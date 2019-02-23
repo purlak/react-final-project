f00ba29fc6fb3bea1121a8d6e56e76e35bfc1cbc
github username <purnima.nyc@gmail.com>
2019-02-23 12:32:09 -0500

    update about page

--- react-client/src/components/About.js
+++ react-client/src/components/About.js
@@ -5,7 +5,8 @@ class About extends Component {
     return (
       <div className="watchlist">
         <p className="headertext">About this App:</p>
-        <p className="text"> This App uses a React/Redux front end and Rails backend. It connects to GoogleBooks API to search for books and allows the user to save the book to their WatchList/BookList. </p>
+        <p className="text"> This App uses a React/Redux front end and a Rails backend. It connects to GoogleBooks API to search for books and allows the user to save the book to their WatchList/BookList. </p>
+        <p className="text"> For more information visit: <a href="https://developers.google.com/books/">Google Books API</a></p>
       </div>
     )
   }
