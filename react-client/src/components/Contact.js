00ca71b7ea24cb08b52d62804b73de3a76d84a23
github username <purnima.nyc@gmail.com>
2019-02-23 12:20:20 -0500

    add contact info

--- react-client/src/components/Contact.js
+++ react-client/src/components/Contact.js
@@ -1,10 +1,18 @@
 import React, { Component  } from 'react';
+import '../App.css';
 
 class Contact extends Component {
 
   render() {
     return (
-
+      <div className="watchlist">
+        <p className="headertext">Contact Information:</p>
+        <p className="text"> XYZ Inc. </p>
+        <p className="text">Avenue at Brooklyn</p>
+        <p className="text">New York, NY</p>
+        <p className="text">Email: <a href="mailto:contact@xyz.com">contact@xyz.com</a></p>
+        <p className="text">Phone: 1800-XYZ-CALL</p>
+      </div>
     )
   }
 }
