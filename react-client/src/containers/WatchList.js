d66558d209f31ac91905204c7d8c16fd90936ab7
github username <purnima.nyc@gmail.com>
2019-03-04 13:45:17 -0500

    add Like component

--- react-client/src/containers/WatchList.js
+++ react-client/src/containers/WatchList.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import DisplayTitle from '../components/DisplayTitle.js';
+import '../App.css';
+import Like from './Like.js';
+
 
 class WatchList extends Component {
+  constructor () {
+    super ();
+    // this.state = {
+    //   counter: 0
+    // }
+  }
+
   render () {
+    // console.log(this.state.counter)
     const { watchList } = this.props
     if (watchList.length === 0) {
       return <p className="text">You do not have any books in your list currently. </p>
@@ -11,7 +22,10 @@ class WatchList extends Component {
     return (
       <div>
         {watchList.map(book => <p>
-          <DisplayTitle title={book.title} />
+          <div className="displayButton">
+            <DisplayTitle title={book.title} />
+            <Like />
+          </div>
         </p>)}
       </div>
     )}
